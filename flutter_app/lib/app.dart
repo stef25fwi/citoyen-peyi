73bac7c61845894b02740afc90acd41d05125f02
stef25fwi <sahai.stephane@gmail.com>
2026-07-22 08:26:40 -0400

    feat: applique le responsive global à toutes les routes

--- flutter_app/lib/app.dart
+++ flutter_app/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'routes/app_router.dart';
 import 'theme/app_theme.dart';
+import 'widgets/app_responsive_viewport.dart';
 
 class CitoyenPeyiApp extends StatelessWidget {
   const CitoyenPeyiApp({super.key});
@@ -12,6 +13,9 @@ class CitoyenPeyiApp extends StatelessWidget {
       title: 'Citoyen Peyi',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
+      builder: (context, child) => AppResponsiveViewport(
+        child: child ?? const SizedBox.shrink(),
+      ),
       onGenerateRoute: AppRouter.onGenerateRoute,
       initialRoute: '/',
     );
